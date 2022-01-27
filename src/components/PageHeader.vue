edb118c7509ae6621725d402d230c058ef590922
CreatorSiSo <64036709+CreatorSiSo@users.noreply.github.com>
2022-01-27 22:40:50 +0100

    Project reset.

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <nav></nav>
+  </header>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'PageHeader',
+    components: {},
+    props: {},
+  });
+</script>
+
+<style></style>
